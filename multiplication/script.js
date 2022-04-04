d0d7979b93d01bfd543c51946ed46c8d2ad183e8
MiKL <mikl.gaillard@gmail.com>
2022-04-04 15:15:12 +0200

    table de multiplication en JS

---
+++ multiplication/script.js
@@ -0,0 +1,31 @@
+// Exercice 2 : Table de multiplication
+// Ecrivez une fonction qui affiche une table de multiplication.
+
+// Votre fonction doit prendre un paramètre qui permet d'indiquer quelle table afficher.
+
+// Par exemple, TableMultiplication(7) doit afficher :
+
+// 1 x 7 = 7
+
+// 2 x 7 = 14
+
+// 3 x 7 = 21 ...
+
+//----------------------------------------------------------------------------//
+
+
+
+let i ;
+let nb = parseInt(prompt("Ecrivez un nombre"));
+
+// table de multiplications
+for (i=0; i<=10; i++)
+{
+    resultat = nb * i ;
+
+    console.log(nb+" * "+i+" = "+resultat) ;
+}
+
+// instructions exécutées après le for (i = 10)
+// affichage du chiffre ou du nombre multiplicateur
+console.log("fin de la table de multiplication par "+nb) ;
