814b9fbd7757bfff8c947020a2854d094675a695
MiKL <mikl.gaillard@gmail.com>
2022-05-09 15:26:17 +0200

    Calcul de la somme d'un tableau

---
+++ Exercie_13_calculDeLaSommeDesValeursDunTableau/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Culcul de la somme des valeurs d'un tableau</title>
+</head>
+<body>
+    <?php
+        // tableau
+        $tab = array(4,3,8,2);
+        // calcul
+        echo array_sum($tab);
+    ?>
+</body>
+</html>
---
+++ Exercie_13_calculDeLaSommeDesValeursDunTableau/readme.md
@@ -0,0 +1,4 @@
+# **Calcul de la somme des valeurs d'un tableau**
+
+    Ecrivez une fonction qui calcul la somme des valeurs d'un tableau
+    La fonction doit prendre un paramètre de type tableau
