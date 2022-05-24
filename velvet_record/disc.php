a977991dc546ce14959b6153bb2fc3465e3e21f1
MiKL <mikl.gaillard@gmail.com>
2022-05-24 09:22:42 +0200

    Velvet record

--- velvet_record/disc.php
+++ velvet_record/disc.php
@@ -1,42 +1,36 @@
-<!-- aller chercher include php en bas du fichier -->
-
-
 <!DOCTYPE html>
 <html lang="fr">
-<!-- Bootstrap -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <title>Liste des disques</title>
 </head>
 
 <body>
 
 <?php include "db.php"; // connexion à la base de données
-
 $db = ConnexionBase(); // connexion
-
-$disques = $db->prepare("SELECT * FROM disc, artist WHERE artist.artist_id = disc.artist_id;;");
+// éviter l'injection SQL
+$disques = $db->prepare("SELECT * FROM disc, artist WHERE artist.artist_id = disc.artist_id;")
 $disques->execute();
-
-
+// Récupèration des lignes restantes d'un ensemble de résultats
 $result = $disques->fetchAll(PDO::FETCH_OBJ);
 // print_r($result); // pour voir si les infos remontent
-
 ?>
 
   <div class="container">
     <div class="row">
-      <div class="col col-11">
+      <div class="col col-8">
         <!-- le nombre de disques doit être calculer pas php par COUNT voir si RECURSIVE (car il faut mettre à jour en temps réel) est une bonne option sinon _NORMAL -->
         <h1 class="font-weight-bold">Liste des disques (<?= count($result) ?>)</h1>
+        <!-- j'ai choisi de compter le nombre de résultats mais j'aurai pu faire une requête SQL qui est plus souvent utiliser -->
       </div> <!-- End of col-11 , liste des disques -->
       <div class="col-1">
         <!-- Un lien vers le formulaire d'ajout doit se trouvé à côté du titre en bout de ligne -->
-        <a class="btn btn-sm btn-success" href="script_artist_ajout.php" role="button">Ajouter</a>
+        <a class="btn btn-sm btn-success" href="script_artist_ajout.php" role="button">Ajouter</a> <!--- vers le formulaire d'ajout (artist_ajout) -->
       </div> <!-- End of col-1 , button 'ajouter' -->
     </div> <!-- End of row -->
   </div> <!-- End of container -->
@@ -44,32 +38,28 @@ $result = $disques->fetchAll(PDO::FETCH_OBJ);
   <!-- deux colonnes contenant deux colonnes -->
   <div class="container">
     <!-- CETTE PORTION DEVRA ÊTRE REPRODUITE PAR PHP À CHAQUE NOUVELLE LIGNE -->
-    
     <div class="row row-cols-1 row-cols-md-2">
     <?php foreach ($result as $disc) : ?>
       <div class="card mb-3 border-0" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-4">
             <img src="img/jaquettes/<?= $disc->disc_picture?>" class="img-fluid rounded" alt="...">
-          </div>
+          </div> <!-- End of col for "jaquette" -->
             <div class="col-md-8">
               <div class="card-body">
                 <h5 class="card-title"><?=$disc->disc_title?></h5>
-                <p>Label : <?=$disc->disc_label?></p>
-                <p>Année : <?=$disc->disc_year?></p>
-                <p>Genre : <?=$disc->disc_genre?></p>
-                <p>Prix : <?=$disc->disc_price?></p>
-                <p>boutton Détail</p>
-              </div>
-            </div>
-          
-        </div>
-      </div>
-        
+                <label><?=$disc->artist_name?></label><br>
+                <label>Label : <?=$disc->disc_label?></label><br>
+                <label>Année : <?=$disc->disc_year?></label><br>
+                <label>Genre : <?=$disc->disc_genre?></label><br>
+                <label>Prix : <?=$disc->disc_price?></label><br>
+                <a href="artist_ajout.php"><button type="button" class="btn btn-sm btn-primary">Détails</button></a> <!--- vers artist_detail -->
+              </div> <!-- End of div card -->
+            </div> <!-- End of div pour les infos des disques -->
+        </div> <!-- End of row gutter (goutière) -->
+      </div> <!-- End of card -->
       <?php endforeach; ?>
-
-    </div> 
-    
+    </div> <!-- End of row row -->
   </div> <!-- End of container -->
 </body>
 
