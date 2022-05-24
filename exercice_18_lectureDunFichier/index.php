7c97108be9563f6cbb312d2b159f16cf5e20c87d
MiKL <mikl.gaillard@gmail.com>
2022-05-24 16:57:37 +0200

    Lecture d'un fichier

--- exercice_18_lectureDunFichier/index.php
+++ exercice_18_lectureDunFichier/index.php
@@ -9,16 +9,27 @@
 </head>
 
 <body>
+    <p> Méthode 1 : la fonction "fopen"</p>
     <?php
     // Ouverture du fichier
-    $fp = fopen("https://ncode.amorce.org/ressources/Pool/MS_FULL_STACK/PHP/src/liens.txt", "r");
-
+    $fp = fopen("https://ncode.amorce.org/ressources/Pool/MS_FULL_STACK/PHP/src/liens.txt", "r"); // r bloque sur read
+    // var_dump($fp);
     while (!feof($fp)) {
         // Lecture d'une ligne, le contenu de la ligne est affecté sous forme de liens
         $ligne = fgets($fp, 4096);
         echo "<a href=>" . $ligne . "<br> </a>";
     }
     ?>
+    <p>Méthode 2 : la fonction "file"</p>
+    <?php
+    // Ouvre, prend le contenun, pusi le libère aux autres utilisateurs
+    $fichier = file("https://ncode.amorce.org/ressources/Pool/MS_FULL_STACK/PHP/src/liens.txt");
+    // var_dump($fichier);
+    foreach ($fichier as $line) {
+        echo "<a href=>" . $line . "<br> </a>";
+    }
+    ?>
+    <!-- "< ? =" uniquement pour une variable ou une fonction pariculière -->
 </body>
 
 </html>
--- exercice_18_lectureDunFichier/readme.md
+++ exercice_18_lectureDunFichier/readme.md
@@ -4,3 +4,6 @@
     Écrire un programme qui lit ce fichier et qui construit une page web contenant une liste de liens hypertextes.
     Utilisez la fonction file() qui permet de lire directement un fichier et renvoie son contenu sous forme de tableau.
     
+    Deux méthode utilisées
+    - "fopen"
+    - et la fonction "file"
