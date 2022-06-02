1d00e946c0dd8b19a8aaff3ef51637f195020940
MiKL <mikl.gaillard@gmail.com>
2022-06-02 14:21:52 +0200

    Password

---
+++ PHP/exercice_15_pswd/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Niveau de compplexité d'un password</title>
+</head>
+
+<body>
+    <?php
+    // Vérif
+    function verifmdp($mdp, &$error)
+    {
+        $error_init = $error;
+
+        // longueur
+        if (strlen($mdp) < 8) {
+            $error[] = "Trop court ! Huit caractères nécéssaires.";
+        }
+        // nombre de chiffre
+        if (!preg_match("#[0-9]+#", $mdp)) {
+            $error[] = "Doit inculre au moins un chiffre entre 0 et 9.";
+        }
+        // nombre de lettre
+        if (!preg_match("#[0-9]+#", $mdp)) {
+            $error[] = "Doit contenir au moins une letre de A à Z.";
+        }
+        // retourne les erreurs
+        return ($error == $error_init);
+    }
+
+    // le mode de passe
+    // $mdp = "b/\thYm3Tr1E";
+    $mdp = "z";
+    $err = [];
+    // Demande de vérifier le mot de passe et stock les messages d'erreur dans la variable $err
+    $result = verifmdp($mdp, $err);
+    // var_dump($err) ;
+    // var_dump($result) ;
+    //Execution
+    if ($result == "true") {
+        echo "Bravo ! Mot de passe sûre.";
+    } else {
+        echo implode("<br> ", $err);
+    }
+    ?>
+</body>
+
+</html>
---
+++ PHP/exercice_15_pswd/readme.md
@@ -0,0 +1,15 @@
+# **Vérifier le niveau de complésité d'un mot de passe**
+
+> ### ***En PHP***
+    Créer une fonction qui vérifie le niveau de complexité d'un mot de passe
+    - doit prendra un paramètre de type chaîne de caractères. 
+    - retournera une valeur booléenne qui vaut true si le paramètre (le mot de passe) respecte les règles suivantes :
+
+    1. Faire au moins 8 caractères de long
+    2. Avoir au moins 1 chiffre
+    3. Avoir au moins une majuscule et une minuscule
+    4. $resultat doit contenir true.
+    
+    Aidez-vous des expressions régulières !
+
+    NOTA BENE : dans le fichier, il y a deux mote de passe est le bon est en commentaire.
