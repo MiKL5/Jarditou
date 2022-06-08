fb8db8ba0005a9228e176c16fbc1035db8cb9fbc
MiKL <mikl.gaillard@gmail.com>
2022-06-08 15:19:36 +0200

    Multiplication

--- javaScript/exercice_02_multiplication/script.js
+++ javaScript/exercice_02_multiplication/script.js
@@ -17,15 +17,12 @@ function tableMultiplication(nb) {
   // table de multiplications
   for (var i = 0; i <= 10; i++) {
     resultat = nb * i;
-
     console.log(nb + " * " + i + " = " + resultat);
   }
-
   // instructions exécutées après le for (i = 10)
   // affichage du chiffre ou du nombre multiplicateur
   console.log("Fin de la table de multiplication par " + nb);
-  window.alert("Fin de la table de multiplication par " + nb);
+  window.alert("Dans la console s'affiche la multiplication par " + nb);
 }
-
 let n = parseInt(prompt("Ecrivez un nombre"));
 tableMultiplication(n);
